d4280531e7dddf388f0dc6764236b24d62476604
dianamgil <gbdianam@gmail.com>
2026-09-25 12:21:37 +0200

    update: schema and add teaching data

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,7 @@ const userCollection = defineCollection({
 
   loader: glob({ pattern: '**/*.json', base: './src/content/user' }), //glob-->Buscador de archivos .json dentro de carpeta src/content/user
   schema: z.object({ //define estructura de datos esperada (reglas de validación de datos Zod)
+
     name: z.string(),
     role: z.string(),
     university: z.string(),
@@ -21,7 +22,26 @@ const userCollection = defineCollection({
       primaryColor: z.string(),
       backgroundColor: z.string()
     }),
+
+    teaching: z.array(
+      z.object({
+        period: z.string().regex(/^\d{4}-\d{0,4}$/, ).refine((val) => {
+    const [startStr, endStr] = val.split('-'); // separa "2020-2024" en ["2020", "2024"], o "2020-" en ["2020", ""]
+    const start = Number(startStr);
+    if (start < 1950 || start > 2100) return false; // año inicial fuera de rango razonable
+    if (endStr) { // si hay año final existe
+      const end = Number(endStr);
+      if (end < 1950 || end > 2100 || end < start) return false; // valida que sea superior al año inicial y dentro de rango
+    }
+    return true; // pasa la validación
+    },),
+    subject: z.string().trim(),
+    role: z.string().trim(),
+    universidad: z.string().trim()
+
        
+      })
+    ),
     links: z.array(
       z.object({
         id: z.string(), // Agregar un campo "id" para cada publicación, Escalable con Nest.js.(repeatable, future CRUD/DB rows)
